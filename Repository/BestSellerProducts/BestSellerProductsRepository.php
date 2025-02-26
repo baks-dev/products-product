26d43edabc18c73161850763c70e660052b56aef
kingstar <admin@baks.dev>
2025-02-27 02:05:01 +0300

    Baks Development

--- Repository/BestSellerProducts/BestSellerProductsRepository.php
+++ Repository/BestSellerProducts/BestSellerProductsRepository.php
@@ -1,17 +1,17 @@
 <?php
 /*
  *  Copyright 2025.  Baks.dev <admin@baks.dev>
- *
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,6 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
- *
  */
 
 declare(strict_types=1);
@@ -59,9 +58,7 @@ final class BestSellerProductsRepository implements BestSellerProductsInterface
 
     private int|false $maxResult = false;
 
-    public function __construct(
-        private readonly DBALQueryBuilder $dbal
-    ) {}
+    public function __construct(private readonly DBALQueryBuilder $dbal) {}
 
     /**
      * Исключает продукт по Product Invariable
@@ -113,7 +110,7 @@ final class BestSellerProductsRepository implements BestSellerProductsInterface
     /**
      * Метод возвращает информацию о самых продаваемых продуктах
      */
-    public function find(): array|false
+    public function findAll(): array|false
     {
         $dbal = $this->dbal
             ->createQueryBuilder(self::class)
