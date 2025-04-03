c9bccdb1be295d6b29c0e8807d990ed7c40cce93
kingstar <admin@baks.dev>
2025-04-03 23:54:05 +0300

    Baks Development

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -38,6 +38,7 @@ return static function(ContainerConfigurator $container) {
         ->exclude([
             BaksDevProductsProductBundle::PATH.'{Entity,Resources,Type}',
             BaksDevProductsProductBundle::PATH.'**'.DIRECTORY_SEPARATOR.'*Message.php',
+            BaksDevProductsProductBundle::PATH.'**'.DIRECTORY_SEPARATOR.'*Result.php',
             BaksDevProductsProductBundle::PATH.'**'.DIRECTORY_SEPARATOR.'*DTO.php',
         ]);
 };
