e5d86af551ce47ff722905adcdf1da9120fb4afe
adminap <shestopalov92@gmail.com>
2025-05-12 22:00:15 +0300

    fork: PR_12.05.2025

--- Repository/Cards/ModelOrProduct/Tests/ModelOrProductRepositoryTest.php
+++ Repository/Cards/ModelOrProduct/Tests/ModelOrProductRepositoryTest.php
@@ -27,6 +27,12 @@ declare(strict_types=1);
 namespace BaksDev\Products\Product\Repository\Cards\ModelOrProduct\Tests;
 
 use BaksDev\Products\Product\Repository\Cards\ModelOrProduct\ModelOrProductInterface;
+use BaksDev\Products\Product\Repository\Cards\ModelOrProduct\ModelOrProductResult;
+use BaksDev\Products\Product\Repository\Cards\ModelsOrProductsByCategory\ModelOrProductByCategoryResult;
+use BaksDev\Products\Product\Type\Event\ProductEventUid;
+use BaksDev\Products\Product\Type\Id\ProductUid;
+use BaksDev\Reference\Currency\Type\Currency;
+use BaksDev\Reference\Money\Type\Money;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\Attribute\When;
 
@@ -41,11 +47,66 @@ class ModelOrProductRepositoryTest extends KernelTestCase
         /** @var ModelOrProductInterface $repository */
         $repository = self::getContainer()->get(ModelOrProductInterface::class);
 
-        $result = $repository
+        $results = $repository
             ->maxResult(10000)
             //        ->analyze()
             ->toArray();
 
-        self::assertTrue(true);
+        self::assertNotEmpty($results);
+
+        /** @var ModelOrProductByCategoryResult $result */
+        foreach($results as $result)
+        {
+            self::assertInstanceOf(ModelOrProductResult::class, $result);
+
+            self::assertInstanceOf(ProductUid::class, $result->getProductId());
+            self::assertInstanceOf(ProductEventUid::class, $result->getProductEvent());
+
+            is_string($result->getProductName()) ?: self::assertNull($result->getProductName());
+
+            is_string($result->getProductUrl()) ?: self::assertNull($result->getProductUrl());
+
+            is_int($result->getProductSort()) ?: self::assertNull($result->getProductSort());
+
+            is_string($result->getProductActiveFrom()) ?: self::assertNull($result->getProductActiveFrom());
+
+            is_bool($result->getCategoryOfferCard()) ?: self::assertNull($result->getCategoryOfferCard());
+            is_string($result->getProductOfferReference()) ?: self::assertNull($result->getProductOfferReference());
+            is_string($result->getProductOfferValue()) ?: self::assertNull($result->getProductOfferValue());
+            self::assertIsString($result->getOfferAgg());
+
+            is_bool($result->getCategoryVariationCard()) ?: self::assertNull($result->getCategoryVariationCard());
+            is_string($result->getProductVariationReference()) ?: self::assertNull($result->getProductVariationReference());
+            is_string($result->getProductVariationValue()) ?: self::assertNull($result->getProductVariationValue());
+            self::assertIsString($result->getVariationAgg());
+
+            is_bool($result->getCategoryModificationCard()) ?: self::assertNull($result->getCategoryModificationCard());
+            is_string($result->getProductModificationReference()) ?: self::assertNull($result->getProductModificationReference());
+            is_string($result->getProductModificationValue()) ?: self::assertNull($result->getProductModificationValue());
+            self::assertIsString($result->getModificationAgg());
+
+            is_array($result->getInvariable()) ?: self::assertNull($result->getInvariable());
+
+            is_array($result->getProductRootImages()) ?: self::assertNull($result->getProductRootImages());
+
+            self::assertIsString($result->getCategoryName());
+
+            is_bool($result->getProductPrice()) ?
+                self::assertFalse($result->getProductPrice()) :
+                self::assertInstanceOf(Money::class, $result->getProductPrice());
+
+            is_bool($result->getProductOldPrice()) ?
+                self::assertFalse($result->getProductOldPrice()) :
+                self::assertInstanceOf(Money::class, $result->getProductOldPrice());
+
+            is_bool($result->getProductCurrency()) ?: self::assertInstanceOf(Currency::class, $result->getProductCurrency());
+
+            is_array($result->getCategorySectionField()) ?: self::assertNull($result->getCategorySectionField());
+            is_int($result->getProductQuantity()) ?: self::assertNull($result->getProductQuantity());
+
+            is_string($result->getProductOfferPostfix()) ?: self::assertNull($result->getProductOfferPostfix());
+            is_string($result->getProductVariationPostfix()) ?: self::assertNull($result->getProductVariationPostfix());
+            is_string($result->getProductModificationPostfix()) ?: self::assertNull($result->getProductModificationPostfix());
+        }
     }
 }
