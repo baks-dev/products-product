ac48d19e6315a6a5a37124e6a2752e19c70b2600
MichelAngelo <baksdevelopmen@gmail.com>
2025-06-27 10:57:18 +0300

    BaksDevelopment

--- Repository/Cards/ModelOrProduct/ModelOrProductRepository.php
+++ Repository/Cards/ModelOrProduct/ModelOrProductRepository.php
@@ -57,11 +57,8 @@ use BaksDev\Products\Product\Entity\Product;
 use BaksDev\Products\Product\Entity\ProductInvariable;
 use BaksDev\Products\Product\Entity\Property\ProductProperty;
 use BaksDev\Products\Product\Entity\Trans\ProductTrans;
-use BaksDev\Users\Profile\UserProfile\Entity\Info\UserProfileInfo;
-use BaksDev\Users\Profile\UserProfile\Repository\UserProfileTokenStorage\UserProfileTokenStorageInterface;
-use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
-use BaksDev\Users\Profile\UserProfile\Type\UserProfileStatus\Status\UserProfileStatusActive;
-use BaksDev\Users\Profile\UserProfile\Type\UserProfileStatus\UserProfileStatus;
+use BaksDev\Users\Profile\UserProfile\Entity\Discount\UserProfileDiscount;
+use BaksDev\Users\Profile\UserProfile\Entity\UserProfile;
 use Generator;
 
 /** @see ModelOrProductResult */
@@ -94,6 +91,11 @@ final class ModelOrProductRepository implements ModelOrProductInterface
     public function toArray(): array|false
     {
         $result = $this->findAll();
+
+        if(false === $result)
+        {
+            return false;
+        }
 
         return (true === $result->valid()) ? iterator_to_array($result) : false;
     }
@@ -697,35 +699,53 @@ final class ModelOrProductRepository implements ModelOrProductInterface
 			END AS product_quantity
 		');
 
-
         /** Персональная скидка из профиля авторизованного пользователя */
-        if(true === $this->userProfileTokenStorage->isUser())
+        if(true === $dbal->bindCurrentProfile())
         {
-            $profile = $this->userProfileTokenStorage->getProfileCurrent();
 
-            if($profile instanceof UserProfileUid)
-            {
-                $dbal
-                    ->addSelect('profile_info.discount AS profile_discount')
-                    ->leftJoin(
-                        'product',
-                        UserProfileInfo::class,
-                        'profile_info',
-                        '
-                        profile_info.profile = :profile AND 
-                        profile_info.status = :profile_status'
-                    )
-                    ->setParameter(
-                        key: 'profile',
-                        value: $profile,
-                        type: UserProfileUid::TYPE)
-                    /** Активный статус профиля */
-                    ->setParameter(
-                        key: 'profile_status',
-                        value: UserProfileStatusActive::class,
-                        type: UserProfileStatus::TYPE
-                    );
-            }
+            $dbal
+                ->join(
+                    'product',
+                    UserProfile::class,
+                    'current_profile',
+                    '
+                        current_profile.id = :'.$dbal::CURRENT_PROFILE_KEY,
+                );
+
+            $dbal
+                ->addSelect('current_profile_discount.value AS profile_discount')
+                ->leftJoin(
+                    'current_profile',
+                    UserProfileDiscount::class,
+                    'current_profile_discount',
+                    '
+                        current_profile_discount.event = current_profile.event
+                        ',
+                );
+        }
+
+        /** Общая скидка (наценка) из профиля магазина */
+        if(true === $dbal->bindProjectProfile())
+        {
+
+            $dbal
+                ->join(
+                    'product',
+                    UserProfile::class,
+                    'project_profile',
+                    '
+                        project_profile.id = :'.$dbal::PROJECT_PROFILE_KEY,
+                );
+
+            $dbal
+                ->addSelect('project_profile_discount.value AS project_discount')
+                ->leftJoin(
+                    'project_profile',
+                    UserProfileDiscount::class,
+                    'project_profile_discount',
+                    '
+                        project_profile_discount.event = project_profile.event',
+                );
         }
 
         /** Только в наличии */
@@ -760,6 +780,9 @@ final class ModelOrProductRepository implements ModelOrProductInterface
         {
             $dbal->setMaxResults($this->maxResult);
         }
+
+
+        //        dd($dbal->fetchAllAssociative());
 
         return $dbal;
     }
