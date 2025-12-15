d57e18ebd3fd3da4d10da511f335756a3bbecbfa
dz <ladovod@gmail.com>
2025-12-15 17:17:15 +0500

    Правит ограничение по загрузке изображений

--- Resources/assets/products-product/product.photo.js
+++ Resources/assets/products-product/product.photo.js
@@ -131,10 +131,14 @@ if($addButtonPhoto)
         let newForm = document.getElementById($addButtonPhoto.dataset.prototype).dataset.prototype;
         let index = $addButtonPhoto.dataset.index * 1;
 
-        if(index === 12)
-        {
+        /** Подсчитать кол-во в момент добавления */
+        const item_collections = document.getElementsByClassName('item-collection-photo');
+        /* Если кол-во элементов равно 12 то не даем возможность добавлять */
+        const total_count = item_collections.length;
+        if (total_count === 12) {
             return;
         }
+
 
         /* Замена '__name__' в HTML-коде прототипа на
          вместо этого будет число, основанное на том, сколько коллекций */
@@ -268,10 +272,14 @@ if($addButtonPhoto)
 
             let index = $addButtonPhoto.dataset.index * 1;
 
-            if(index === 12)
-            {
+            /** Подсчитать кол-во в момент добавления */
+            const item_collections = document.getElementsByClassName('item-collection-photo');
+            /* Если кол-во элементов равно 12 то не даем возможность добавлять */
+            const total_count = item_collections.length;
+            if (total_count === 12) {
                 return;
             }
+
 
             /* Заменить '__name__' в HTML-коде прототипа числом, основанным на том, сколько коллекций */
             newPrototype = newPrototype.replace(/__photos__/g, index);
